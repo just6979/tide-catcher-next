b4e8f28c6fa26b5a694b5a08685b9c8df87f9a4b
Justin White <jw@justinwhite.net>
2025-09-26 12:59:08 -0400

    clean up by-location routes

--- app/api/tides/by-location/[...location]/route.ts
+++ app/api/tides/by-location/[...location]/route.ts
@@ -1,15 +1,11 @@
-import {NextRequest, NextResponse} from "next/server";
-
-export async function GET(request: NextRequest, {params}: { params: Promise<{ location: string }> }) {
-  console.log("incoming request: " + request.url)
-
-  const {location} = await params;
-
-  const old_api_url = 'https://tide-catcher.appspot.com/json/tides/by-location/'.concat(location);
-  console.log(old_api_url)
-
-  const old_api_response = await fetch(old_api_url, {cache: 'force-cache'})
-  const old_api_data = await old_api_response.json();
-
-  return NextResponse.json(old_api_data)
+export async function GET(request: Request, {params}: { params: Promise<{ location: string }> }) {
+  let location = (await params).location;
+  const external_response = await fetch(
+    'https://tide-catcher.appspot.com/json/tides/by-location/' + location,
+    {cache: 'force-cache'}
+  )
+  const data = await external_response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
