d90573caaca278a52fce58309af078545e0726b9
Justin White <jw@justinwhite.net>
2025-09-27 00:12:45 -0400

    update metadata, remove Head, add Scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type {Metadata} from "next"
 import "./globals.css"
-import Head from "@/app/head"
+import Script from "next/script";
 import Header from '@/app/header'
 import {Suspense} from "react";
 import Loading from "@/app/loading";
 import Footer from '@/app/footer'
 
 export const metadata: Metadata = {
-  title: "Tide Catcher Next",
-  description: "Tides served with Next.js",
+  title: "Tide Catcher",
+  description: "Web app to quickly display upcoming high & low tide data from nearest NOAA tide stations.",
 }
 
 export default function RootLayout({
@@ -18,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head/>
+      <Script src="/static/js/contrib/jquery-3.1.0.min.js"/>
+      <Script src="/static/js/contrib/underscore-1.8.3-min.js"/>
+      <Script src="/static/js/contrib/backbone-1.3.3-min.js"/>
+      <Script src="/static/js/contrib/mustache-2.2.1.min.js"/>
+      <Script src="/static/js/index.js"/>
       <body>
         <Header/>
         <Suspense fallback={<Loading/>}>
