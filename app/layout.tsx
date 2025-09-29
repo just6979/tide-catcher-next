00a00216e21c9629e9add2614713917c9493fed6
Justin White <jw@justinwhite.net>
2025-09-29 10:40:04 -0400

    update metadata and small clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,32 @@
-import type {Metadata} from "next"
-import "./globals.css"
+import type {Metadata, Viewport} from "next"
 import Script from "next/script";
+import {ReactNode, Suspense} from "react";
+
 import Header from '@/app/header'
-import {Suspense} from "react";
 import Loading from "@/app/loading";
 import Footer from '@/app/footer'
 
+import "./globals.css"
+
 export const metadata: Metadata = {
-  title: "Tide Catcher",
-  description: "Web app to quickly display upcoming high & low tide data from nearest NOAA tide stations.",
+  title: {
+    default: 'Tide Catcher',
+    template: '%s | Tide Catcher'
+  },
+  applicationName: 'Tide Catcher',
+  description: 'Web app to quickly display upcoming high & low tide data from nearest NOAA tide stations.',
+  generator: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+}
+
+export const viewport: Viewport = {
+  themeColor: 'lightblue'
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
