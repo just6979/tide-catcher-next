79a3806c99bbe3eecee65f06057fdeab3d41edd4
Justin White <jw@justinwhite.net>
2025-09-29 21:38:06 -0400

    add an api route for all stations list

--- app/api/stations/route.ts
+++ app/api/stations/route.ts
@@ -1,10 +1,24 @@
 export async function GET() {
   const external_response = await fetch(
-    'https://tide-catcher.appspot.com/json/stations',
-    {cache: 'force-cache'}
+    'https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions'
   )
-  const data = await external_response.json();
-  return new Response(JSON.stringify(data), {
-    headers: { 'Content-Type': 'application/json' },
+  const in_data = await external_response.json();
+
+  const stations = in_data['stations'].map((station: {
+    id: string,
+    name: string,
+    lat: number,
+    lng: number
+  } ) => ({
+    id: station.id,
+    name: station.name,
+    lat: station.lat,
+    lon: station.lng
+  }));
+
+  const out_data = {count: in_data['count'], stations: stations};
+
+  return new Response(JSON.stringify(out_data), {
+    headers: {'Content-Type': 'application/json'},
   });
 }
