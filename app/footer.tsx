6b6ae242c154aa4c2587077569e4d533ae23afb7
Justin White <jw@justinwhite.net>
2025-09-30 23:51:58 -0400

    remove worldtides.info copyright

--- app/footer.tsx
+++ app/footer.tsx
@@ -26,18 +26,6 @@ export default function Footer() {
           </li>
         </ul>
       </div>
-
-      <div id="notes">
-        <p>
-          <em>
-            Tidal data retrieved from www.worldtides.info. Copyright (c) 2014-2025 Brainware LLC. Licensed for
-            use of individual spatial coordinates on behalf of/by an end-user. Source data created by the Center
-            for Operational Oceanographic Products and Services (CO-OPS) and is not subject to copyright
-            protection. NO GUARANTEES ARE MADE ABOUT THE CORRECTNESS OF THIS DATA. You may not use it if anyone
-            or anything could come to harm as a result of using it (e.g. for navigational purposes).
-          </em>
-        </p>
-      </div>
     </div>
   )
 }
