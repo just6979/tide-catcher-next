8a7d6d59523ee85b8397a8af14b766ec6a6870e2
Justin White <jw@justinwhite.net>
2025-09-30 22:55:37 -0400

    better handle no nearby stations found

--- app/lib/stations/nearby.ts
+++ app/lib/stations/nearby.ts
@@ -2,7 +2,6 @@ export default async function nearby(location: string, initial_range: number = 1
   const lat = location.split(',')[0]
   const lon = location.split(',')[1]
 
-  let station_id = null
   let range = initial_range
   let attempts = 4
 
@@ -17,12 +16,17 @@ export default async function nearby(location: string, initial_range: number = 1
     const stations_list = nearby_data['stationList']
     if (stations_list != null) {
       const first_station = stations_list[0]
-      station_id = first_station['stationId']
-      return station_id
+      const station_id = first_station['stationId']
+      return {
+        station_id: station_id,
+      }
 
     }
     attempts -= 1
     range *= 2
   }
-  return null
+
+  return {
+    error: `No stations found within ${range} miles of location (${location}).`
+  }
 }
--- app/lib/tides/by_location.ts
+++ app/lib/tides/by_location.ts
@@ -2,11 +2,11 @@ import nearby from "@/app/lib/stations/nearby"
 import by_station from "@/app/lib/tides/by_station";
 
 export default async function by_location(location: string) {
-  const station_id = await nearby(location)
-  if (station_id == null) {
-    return {
-      'error': `No station found near location (${location}).`
-    }
+  const nearby_data = await nearby(location)
+  if ('error' in nearby_data) {
+    return nearby_data
   }
+
+  const station_id = nearby_data['station_id']
   return await by_station(station_id)
 }
