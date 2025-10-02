bf7793e816c4859abdf77653ca7d9d63d267cfe8
Justin White <jw@justinwhite.net>
2025-10-01 23:59:18 -0400

    add api/station/location endpoint to get only one station by location

---
+++ app/api/station/location/[...location]/route.ts
@@ -0,0 +1,10 @@
+import stationsFromLocation from "@/app/lib/stationsFromLocation"
+
+export async function GET(request: Request, {params}: { params: Promise<{ location: string }> }) {
+  const location = (await params).location[0]
+  const responseData = await stationsFromLocation(location, 1)
+
+  return new Response(JSON.stringify(responseData), {
+    headers: {'Content-Type': 'application/json'},
+  })
+}
