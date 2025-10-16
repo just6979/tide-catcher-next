cf970c6ad8302b2d0ee2393e416fe85b7c66f278
Justin White <jw@justinwhite.net>
2025-10-16 14:18:36 -0400

    add a few checks and logging to `coords.ts`

--- app/lib/coords.ts
+++ app/lib/coords.ts
@@ -3,21 +3,32 @@ import {Coords} from '@/app/lib/types'
 export const ZERO_COORDS: Coords = coordsFromLatLon(0, 0)
 
 export function coordsFromLatLon(lat: number, lon: number): Coords {
+  if (lat < -90 || lat > 90) {
+    console.error(`coordsFromString: Invalid latitude: ${lat}\`)`)
+    return ZERO_COORDS
+  }
+  if (lon < -180 || lon > 180) {
+    console.warn(`coordsFromString: Potentially invalid longitude: ${lon}\`)`)
+    // don't error out on longitude because the range is more geopolitical than physical
+  }
   return {lat, lon}
 }
 
 export function coordsFromString(locationString: string): Coords {
   const location = locationString.split(',')
   if (location.length != 2) {
+    console.error(`coordsFromString: Invalid location string, expect comma-separated numbers: ${location}`)
     return ZERO_COORDS
   }
-  return coordsFromLatLon(Number(location[0]), Number(location[1]))
+  const lat = Number(location[0])
+  const lon = Number(location[1])
+  if (Number.isNaN(lat) || Number.isNaN(lon)) {
+    console.error(`coordsFromString: Invalid location string: expected 2 valid numbers: ${location}`)
+    return ZERO_COORDS
+  }
+  return coordsFromLatLon(lat, lon)
 }
 
 export function coordsToString(coords: Coords): string {
   return `${coords.lat.toFixed(5)},${coords.lon.toFixed(5)}`
-}
-
-export function coordsToBracketString(coords: Coords): string {
-  return `[${coordsToString(coords)}]`
 }
