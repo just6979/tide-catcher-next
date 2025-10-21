7a023a0b360c1d5aca6c210bee5b8d471da1f851
Justin White <jw@justinwhite.net>
2025-10-21 16:11:47 -0400

    add external link icon and remove decoration on external links

--- app/components/TidesElement.tsx
+++ app/components/TidesElement.tsx
@@ -1,7 +1,9 @@
 import {coordsToString} from '@/app/lib/coords'
 import type {Tide, TidesResponse} from '@/app/lib/types'
+import {faArrowUpRightFromSquare} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-export default function TidesElement(props: {data: TidesResponse, nowDate: Date}) {
+export default function TidesElement(props: { data: TidesResponse, nowDate: Date }) {
   const {data, nowDate} = props
   const reqLoc = coordsToString(data.reqLocation)
   const stationLoc = coordsToString(data.station.location)
@@ -35,7 +37,10 @@ export default function TidesElement(props: {data: TidesResponse, nowDate: Date}
         {data.reqLocation != undefined &&
           <tr>
             <td>
-              <a href={`https://www.google.com/maps/place/${reqLoc}/@${reqLoc},12z`} target="_blank">Your Location</a>
+              <a href={`https://www.google.com/maps/place/${reqLoc}/@${reqLoc},12z`}
+                 className="external" target="_blank">
+                Your Location <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>
+              </a>
             </td>
             <td>
               [<a href={`/tides/location/${reqLoc}`}>{reqLoc}</a>]
@@ -45,15 +50,21 @@ export default function TidesElement(props: {data: TidesResponse, nowDate: Date}
         <tr>
           <td>
             <a href={`https://tidesandcurrents.noaa.gov/noaatidepredictions.html?id=${(data.station.id)}`}
-               target="_blank">Station ID</a>
+               className="external" target="_blank">
+              Station ID <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>
+            </a>
           </td>
           <td>
             <a href={`/tides/station/${data.station.id}`}>{data.station.id}</a>
           </td>
         </tr>
         <tr>
-          <td><a href={`https://www.google.com/maps/place/${stationLoc}/@${stationLoc},12z`}
-                 target="_blank">Station Location</a>
+          <td>
+            <a href={`https://www.google.com/maps/place/${stationLoc}/@${stationLoc},12z`}
+               className="external" target="_blank">
+              Station Location <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>
+            </a>
+
           </td>
           <td>
             [<a href={`/tides/location/${stationLoc}`}>{stationLoc}</a>]
