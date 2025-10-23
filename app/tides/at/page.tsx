cb7d384d496ffb1d9a037d17d08e9d85960a720f
Justin White <jw@justinwhite.net>
2025-10-23 13:53:30 -0400

    make the mode selector interactive, add `/at?` param-based page to handle selections
    Should the purely path-based pages (eg /tides/station/[station] go away?
    Or should `/at?` go away and the selector use the path-based pages?
    Or keep both?
    Doing it the param way was too simple not to try for a first go.

---
+++ app/tides/at/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import TidesFromLocationElement from "@/app/components/TidesFromLocationElement"
+import TidesFromStationElement from "@/app/components/TidesFromStationElement"
+import { coordsFromString } from "@/app/lib/coords"
+import TidesFromGeolocation from "@/app/tides/gps/page"
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+import { JSX } from "react"
+
+export default function TidesAt() {
+  const searchParams = useSearchParams()
+  const location = searchParams.get("location")
+  const station = searchParams.get("station")
+
+  if (location) {
+    if (location == "gps") {
+      return <TidesFromGeolocation />
+    }
+    if (coordsFromString(location)) {
+      return <TidesFromLocationElement location={location} />
+    }
+    return makeError(`Invalid location: ${location}`)
+  }
+
+  if (station) {
+    if (station.length == 7) {
+      return <TidesFromStationElement id={station} />
+    }
+    return makeError(`Invalid station: ${station}`)
+  }
+
+  return makeError("No valid Station or Location provided.")
+}
+
+function makeError(msg: string): JSX.Element {
+  return (
+    <div>
+      <p>{msg}</p>
+      <p>
+        <Link href="/tides">Go Back</Link>
+      </p>
+    </div>
+  )
+}
--- app/tides/page.tsx
+++ app/tides/page.tsx
@@ -1,22 +1,24 @@
-import Link from "next/link"
+import Form from "next/form"
 
-export default async function Tides() {
+export default async function TidesChooser() {
   return (
     <div id="mode-selector">
       <h3>How would you like to get your tides?</h3>
-      <ul>
-        <li>
-          <Link href="/tides/gps">Nearby via GPS</Link>
-        </li>
-        <li>
-          <Link href="/tides/location/42.710,-70.788">
-            Near a given location
-          </Link>
-        </li>
-        <li>
-          <Link href="/tides/station/8441241">From a specific station</Link>
-        </li>
-      </ul>
+      <Form action="/tides/at">
+        Nearby via GPS:&nbsp;
+        <input name="location" value="gps" type="hidden" />
+        <button type="submit">Go</button>
+      </Form>
+      <Form action="/tides/at">
+        Near a given location:&nbsp;
+        <input name="location" defaultValue="42.710,-70.788" />
+        <button type="submit">Go</button>
+      </Form>
+      <Form action="/tides/at">
+        From a specific station:&nbsp;
+        <input name="station" defaultValue="8441241" />
+        <button type="submit">Go</button>
+      </Form>
     </div>
   )
 }
