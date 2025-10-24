057ef518ac4941c80297185c27813b491d6d703d
Justin White <jw@justinwhite.net>
2025-10-24 00:56:10 -0400

    fix some missing `type` modifiers on imports

--- app/api/station/location/[[...loc]]/route.ts
+++ app/api/station/location/[[...loc]]/route.ts
@@ -1,7 +1,7 @@
 import { defaultLocation } from "@/app/_lib/constants"
 import { coordsFromString } from "@/app/_lib/coords"
 import { stationsFromCoords } from "@/app/_lib/stationsFromCoords"
-import { StationsResponse } from "@/app/_lib/types"
+import type { StationsResponse } from "@/app/_lib/types"
 import { UTCDate } from "@date-fns/utc"
 
 export async function GET(
