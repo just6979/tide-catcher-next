d3a89766454c14936f7ad720216ecd2a5722ded8
Justin White <jw@justinwhite.net>
2025-10-23 23:13:53 -0400

    make the mode selector even more reactive

--- app/tides/page.tsx
+++ app/tides/page.tsx
@@ -1,24 +1,105 @@
-import Form from "next/form"
+"use client"
 
-export default async function TidesChooser() {
+import { GEOLOCATION_ERRORS } from "@/app/lib/constants"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export default function TidesChooser() {
+  const router = useRouter()
+  const [gpsLocated, setGpsLocated] = useState(false)
+  const [gpsLocation, setGpsLocation] = useState("Locating...")
+  const [location, setLocation] = useState("42.710,-70.788")
+  const [station, setStation] = useState("8441241")
+
+  useEffect(() => {
+    console.log("getting location")
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const loc = `${position.coords.latitude.toFixed(3)},${position.coords.longitude.toFixed(3)}`
+        setGpsLocation(loc)
+        setGpsLocated(true)
+        console.log(`found location: [${loc}]`)
+      },
+      (error) => {
+        console.log(`error getting location: ${GEOLOCATION_ERRORS[error.code]}`)
+        setGpsLocation("Unable to get location")
+      },
+      {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 5000,
+      },
+    )
+  }, [])
+
   return (
     <div id="mode-selector">
-      <h3>How would you like to get your tides?</h3>
-      <Form action="/tides/at">
-        Nearby via GPS:&nbsp;
-        <input name="location" value="gps" type="hidden" />
-        <button type="submit">Go</button>
-      </Form>
-      <Form action="/tides/at">
-        Near a given location:&nbsp;
-        <input name="location" defaultValue="42.710,-70.788" />
-        <button type="submit">Go</button>
-      </Form>
-      <Form action="/tides/at">
-        From a specific station:&nbsp;
-        <input name="station" defaultValue="8441241" />
-        <button type="submit">Go</button>
-      </Form>
+      <p>
+        <strong>
+          How would you like
+          <br />
+          to get your tides?
+        </strong>
+      </p>
+      <p>
+        Nearby:&nbsp;
+        <input
+          id="gps-location"
+          name="location"
+          placeholder={gpsLocation}
+          readOnly
+          onKeyDown={(event) => {
+            if (event.key === "Enter") goToGps()
+          }}
+        />
+        <button onClick={() => goToGps()}>Go</button>
+      </p>
+
+      <p>
+        <span>Location:&nbsp;</span>
+        <span>
+          <input
+            name="location"
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onFocus={(event) => {
+              event.target.select()
+            }}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") goToLocation()
+            }}
+          />
+          <button onClick={() => goToLocation()}>Go</button>
+        </span>
+      </p>
+
+      <p>
+        Station:&nbsp;
+        <input
+          name="station"
+          value={station}
+          onChange={(event) => setStation(event.target.value)}
+          onFocus={(event) => {
+            event.target.select()
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") goToStation()
+          }}
+        />
+        <button onClick={() => goToStation()}>Go</button>
+      </p>
     </div>
   )
+
+  function goToGps(): void {
+    router.push(`/tides/at?location=${gpsLocated ? gpsLocation : "gps"}`)
+  }
+
+  function goToLocation(): void {
+    router.push(`/tides/at?location=${location}`)
+  }
+
+  function goToStation(): void {
+    router.push(`/tides/at?station=${station}`)
+  }
 }
