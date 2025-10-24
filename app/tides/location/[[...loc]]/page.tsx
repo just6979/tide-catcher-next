1210ae49ad3753396f9f79d574f6110ab5e57656
Justin White <jw@justinwhite.net>
2025-10-24 00:36:32 -0400

    `/tides/*/*` routes get better error handling, and "gps" as a special location

--- app/tides/location/[[...loc]]/page.tsx
+++ app/tides/location/[[...loc]]/page.tsx
@@ -2,11 +2,37 @@
 
 import TidesFromLocationElement from "@/app/components/TidesFromLocationElement"
 import { defaultLocation } from "@/app/lib/constants"
+import { coordsFromString } from "@/app/lib/coords"
+import TidesFromGeolocation from "@/app/tides/gps/page"
+import Link from "next/link"
 import { useParams } from "next/navigation"
 
 export default function TidesFromLocation() {
   const { loc } = useParams<{ loc: string }>()
-  const location =
-    loc && loc.length > 0 ? decodeURIComponent(loc[0]) : defaultLocation
-  return <TidesFromLocationElement location={location} />
+
+  if (!loc) {
+    return <TidesFromLocationElement location={defaultLocation} />
+  }
+
+  const location = decodeURIComponent(loc[0])
+
+  if (location === "gps") {
+    return <TidesFromGeolocation />
+  }
+
+  if (coordsFromString(location)) {
+    return <TidesFromLocationElement location={location} />
+  }
+
+  return (
+    <div>
+      <p>
+        <span className="error">Error:</span>
+        Invalid location &ldquo;{location}&rdquo;
+      </p>
+      <p>
+        <Link href="/">Start Over</Link>
+      </p>
+    </div>
+  )
 }
--- app/tides/station/[[...id]]/page.tsx
+++ app/tides/station/[[...id]]/page.tsx
@@ -2,10 +2,30 @@
 
 import TidesFromStationElement from "@/app/components/TidesFromStationElement"
 import { defaultStation } from "@/app/lib/constants"
+import Link from "next/link"
 import { useParams } from "next/navigation"
 
 export default function TidesFromStation() {
   const { id } = useParams<{ id: string }>()
   const stationId = id && id.length > 0 ? id[0] : defaultStation
-  return <TidesFromStationElement id={stationId} />
+
+  if (!stationId) {
+    return <TidesFromStationElement id={defaultStation} />
+  }
+
+  if (stationId.length === 7) {
+    return <TidesFromStationElement id={stationId} />
+  }
+
+  return (
+    <div>
+      <p>
+        <span className="error">Error:</span>
+        Invalid station &ldquo;{stationId}&rdquo;
+      </p>
+      <p>
+        <Link href="/">Start Over</Link>
+      </p>
+    </div>
+  )
 }
