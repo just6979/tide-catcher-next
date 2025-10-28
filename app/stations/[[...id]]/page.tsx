0105b9a4f7a4783aa837c5947578ae095e2720a4
Justin White <jw@justinwhite.net>
2025-10-28 13:31:14 -0400

    make station details page SSR, generate page for the default station

--- app/stations/[[...id]]/page.tsx
+++ app/stations/[[...id]]/page.tsx
@@ -1,39 +1,24 @@
-"use client"
-
-import { DEFAULT_STATION, EMPTY_STATION_RESPONSE } from "@/app/_lib/constants"
+import { DEFAULT_STATION } from "@/app/_lib/constants"
 import { coordsToString } from "@/app/_lib/coords"
-import type { Station, StationsResponse } from "@/app/_lib/types"
-import { useParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import { stationsById } from "@/app/_lib/stationsLocal"
+import type { Station } from "@/app/_lib/types"
 
-export default function StationFromStation() {
-  const { id } = useParams<{ id: string }>()
+export async function generateStaticParams() {
+  return [{ id: [DEFAULT_STATION] }]
+}
+
+export default async function StationFromStation({
+  params,
+}: {
+  params: Promise<{ id?: string[] | undefined }>
+}) {
+  const { id } = await params
   const stationId = id && id.length > 0 ? id[0] : DEFAULT_STATION
 
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState(EMPTY_STATION_RESPONSE)
+  const data = await stationsById(stationId)
 
-  useEffect(() => {
-    fetch(`/api/stations/${stationId}`)
-      .then((res) => res.json())
-      .then((data: StationsResponse) => {
-        setData(data)
-        setIsLoading(false)
-      })
-  }, [stationId])
-
-  if (isLoading) {
-    return <p>Loading Station with ID {stationId}...</p>
-  }
   if (!data || !("stations" in data) || !(data.stations.length > 0)) {
     return <p>No Station with ID {stationId} found.</p>
-  }
-  if (data.status.code != 200) {
-    return (
-      <p>
-        Error: {data.status.code}: {data.status.msg}
-      </p>
-    )
   }
 
   const stationItem: Station = data.stations[0]
