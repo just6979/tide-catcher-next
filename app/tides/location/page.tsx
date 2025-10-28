61ea4bcc4103c4ad24fa035a6bd85bb5190ae017
Justin White <jw@justinwhite.net>
2025-10-28 15:47:14 -0400

    adjust /tides/*/ default routes to avoid name shadowing

--- app/tides/location/page.tsx
+++ app/tides/location/page.tsx
@@ -3,6 +3,6 @@
 import TidesFromLocationElement from "@/app/_components/TidesFromLocationElement"
 import { DEFAULT_LOCATION } from "@/app/_lib/constants"
 
-export default function TidesFromLocation() {
+export default function TidesFromLocationDefault() {
   return <TidesFromLocationElement location={DEFAULT_LOCATION} />
 }
--- app/tides/station/page.tsx
+++ app/tides/station/page.tsx
@@ -3,6 +3,6 @@
 import TidesFromStationElement from "@/app/_components/TidesFromStationElement"
 import { DEFAULT_STATION } from "@/app/_lib/constants"
 
-export default function TidesFromStation() {
+export default function TidesFromStationDefault() {
   return <TidesFromStationElement id={DEFAULT_STATION} />
 }
