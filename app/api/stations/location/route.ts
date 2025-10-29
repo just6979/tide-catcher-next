12419addfe58ef722434f44b1c5bfe697775a263
Justin White <jw@justinwhite.net>
2025-10-29 12:05:51 -0400

    better way to pass query string to a redirect

--- app/api/stations/location/route.ts
+++ app/api/stations/location/route.ts
@@ -1,14 +1,11 @@
 import { DEFAULT_LOCATION } from "@/app/_lib/constants"
-import { getStationLocationParams } from "@/app/api/stations/location/[loc]/route"
 import { redirect, RedirectType } from "next/navigation"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const { countParam, count, rangeParam, range } =
-    getStationLocationParams(request)
-
-  let params: string = countParam ? `&count=${count}` : ""
-  params += rangeParam ? `&range=${range}` : ""
-
-  redirect(`location/${DEFAULT_LOCATION}?${params}`, RedirectType.replace)
+  const params = request.nextUrl.searchParams.toString()
+  redirect(
+    `location/${DEFAULT_LOCATION}${params ? `?${params}` : ""}`,
+    RedirectType.replace,
+  )
 }
