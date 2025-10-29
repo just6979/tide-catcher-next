0954180e93a93dca590aff6c1ce98ea42e749538
Justin White <jw@justinwhite.net>
2025-10-29 13:07:57 -0400

    force-static all the bare path redirects

--- app/api/tides/station/route.ts
+++ app/api/tides/station/route.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_STATION } from "@/app/_lib/constants"
 import { redirect, RedirectType } from "next/navigation"
 
-// export const dynamic = "force-static"
+export const dynamic = "force-static"
 
 export async function GET() {
   redirect(`station/${DEFAULT_STATION}`, RedirectType.replace)
--- app/tides/location/route.ts
+++ app/tides/location/route.ts
@@ -1,6 +1,8 @@
 import { DEFAULT_LOCATION } from "@/app/_lib/constants"
 import { redirect, RedirectType } from "next/navigation"
 
+export const dynamic = "force-static"
+
 export async function GET() {
   redirect(`location/${DEFAULT_LOCATION}`, RedirectType.replace)
 }
